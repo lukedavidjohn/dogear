8ecefe56cf1073633b3fdb3af035cf237d6ed582
Luke Rushworth <lukerushworth@Lukes-MacBook-Pro.local>
2020-07-06 10:19:36 +0100

    adds listener for filterOnChange function debugging

--- public/background.js
+++ public/background.js
@@ -2,4 +2,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendMessage) => {
   if (request.type === "getBookmarks") {
     console.log(request.bookmarks);
   }
+  if (request.type === "filterOnChange") {
+    console.log(request.filteredBookmarks);
+  }
 });
