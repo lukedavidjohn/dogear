fb3fcfe717f06797dc96fd3142f27ed60e795375
Luke Rushworth <ldjrushworth@icloud.com>
2020-07-06 10:27:24 +0100

    adds bookmark filter function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 /*global chrome*/
 import React, { Component } from "react";
 import { getBookmarks } from "./utils/getBookmarks";
+import { searchFilter } from "./utils/searchFilter";
 import SearchResults from "./components/SearchResults";
 
 class App extends Component {
   state = {
     activeSuggestion: 0,
     bookmarks: [],
+    bookmarksOnMount: [],
   };
+
+  filterOnChange = (event) => {
+    const { value } = event.target;
+    const { bookmarksOnMount } = this.state;
+    const filteredBookmarks = searchFilter(bookmarksOnMount, value);
+    // chrome.runtime.sendMessage({
+    //   type: "filterOnChange",
+    //   filteredBookmarks,
+    // });
+    this.setState({
+      bookmarks: filteredBookmarks,
+    });
+  };
+
   componentDidMount() {
     getBookmarks((bookmarks) => {
       // sendMessage is for debugging
       // chrome.runtime.sendMessage({ type: "getBookmarks", bookmarks });
       this.setState({
         bookmarks,
+        bookmarksOnMount: bookmarks,
       });
     });
   }
 
   render() {
+    const { filterOnChange } = this;
     const { activeSuggestion, bookmarks } = this.state;
     return (
       <div>
@@ -28,6 +46,7 @@ class App extends Component {
         <SearchResults
           activeSuggestion={activeSuggestion}
           bookmarks={bookmarks}
+          filterOnChange={filterOnChange}
         />
       </div>
     );
