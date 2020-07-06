1968a788573ec19749889cb7111b29d40c6d1261
Luke Rushworth <ldjrushworth@icloud.com>
2020-07-06 12:41:17 +0100

    removes redundant debugging console.log

--- public/background.js
+++ public/background.js
@@ -1,8 +1,6 @@
 chrome.runtime.onMessage.addListener((request, sender, sendMessage) => {
   if (request.type === "getBookmarks") {
-    console.log(request.displayTree);
   }
   if (request.type === "filterOnChange") {
-    console.log(request.filteredBookmarks);
   }
 });
