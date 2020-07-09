6eb520606446ef504a29ad23e161f32fbfd38f98
Luke Rushworth <ldjrushworth@icloud.com>
2020-07-09 19:25:43 +0100

    adds background function and png files to replace icon with light version when browser in dark mode

--- public/background.js
+++ public/background.js
@@ -1,6 +1,18 @@
+chrome.runtime.onInstalled.addListener(() => {
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    chrome.browserAction.setIcon({
+      path: "./dogear-white-128.png",
+    });
+  }
+});
+
 chrome.runtime.onMessage.addListener((request, sender, sendMessage) => {
   if (request.type === "getBookmarks") {
+    // console.log(request.message);
   }
   if (request.type === "filterOnChange") {
+  }
+  if (request.type === "onKeyDown") {
+    // console.log(request.key);
   }
 });
